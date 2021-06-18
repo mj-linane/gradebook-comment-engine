dde6643a84c666203c4aacd3ca45b367456348cd
MJ <mjlinane@oldrochester.org>
2021-06-18 15:13:07 -0400

    wip

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// Put the inputs into an array, then build the string, adding "and" before the array.length-1
+window.addEventListener('load', () => {
+  // Get DOM Elements
+  copyBtn = document.getElementById('copy-btn')
+  generateBtn = document.getElementById('generate-btn')
+  output = document.getElementById('output')
+
+
+  // Add Event Listeners
+  copyBtn.addEventListener('click', copy)
+  generateBtn.addEventListener('click', formatOutput)
+})
+
+// Actions
+function formatOutput(e) {
+  e.preventDefault()
+
+  firstName = document.getElementById('name')
+  let qualities = getAllCheckboxes()
+  let comment = `Congratulations ${firstName} for completing the course. You were great at ${qualities[0]} and ${qualities[1]}.`
+  output.value = comment
+}
+
+function getCollegeRecommendation() {
+  return recommendation
+}
+
+function getAllCheckboxes() {
+  let checkboxes = document.querySelectorAll('input')
+  qualities = []
+
+  for (let index = 0; index < checkboxes.length; index++) {
+    let checkbox = checkboxes[index]
+    if (checkbox.checked) {
+      qualities.push(checkbox.value)
+    }
+  }
+  return qualities
+}
+
+function copy() {
+    /* Select the text field */
+    output.select()
+    output.setSelectionRange(0, 99999) /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    document.execCommand('copy')
+
+    /* Alert the copied text */
+    alert('Copied the text: ' + output.value)
+
+    /* Clear the output */
+    output.value = ""
+  }
